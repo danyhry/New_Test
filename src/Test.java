116144b7cc36217de4e2d32cfbcbb6b660b67be3
Danigri-source <dankagrigorev@gmail.com>
2021-05-04 17:09:39 +0300

    Added CallableAndFuture

--- src/Test.java
+++ src/Test.java
@@ -2,16 +2,7 @@
 
 public class Test {
     public static void main(String[] args) {
-        int x = 153;
-        int invertedNumber = 0;
-        int copyOfX = x;
 
-        while(copyOfX != 0) {
-            invertedNumber = invertedNumber * 10 + copyOfX % 10; // 351
-            copyOfX = copyOfX / 10;
-        }//
-
-        System.out.println(invertedNumber == x);
     }
 }
 
