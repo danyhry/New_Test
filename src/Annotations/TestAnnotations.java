7b71038e1bdfe6b4ff20466258b248853554abf7
Danigri-source <dankagrigorev@gmail.com>
2021-05-06 16:52:27 +0300

    Added Annotations

---
+++ src/Annotations/TestAnnotations.java
@@ -0,0 +1,8 @@
+package Annotations;
+
+public class TestAnnotations {
+    @MethodInfo(purpose = "Print Hello World")
+    public void printHelloWorld() {
+        System.out.println("Hello world!");
+    }
+}
